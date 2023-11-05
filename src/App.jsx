edd950c7d3a706fb708d4686d44462eb7cbfb97f
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-11-05 13:36:03 +0530

    -small refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
+import { getNotes } from './requests'
+
 const App = () => {
   const addNote = async (event) => {
     event.preventDefault()
@@ -14,7 +15,7 @@ const App = () => {
 
   const result = useQuery({
     queryKey: ['notes'],
-    queryFn: () => axios.get('http://localhost:3001/notes').then(res => res.data)
+    queryFn: getNotes
   })
   console.log(JSON.parse(JSON.stringify(result)))
 
